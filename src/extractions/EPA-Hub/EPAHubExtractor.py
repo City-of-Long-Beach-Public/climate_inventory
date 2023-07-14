a5eae7aad470141cdcc9fc46d9369ab2454ca9e9
Federico Domínguez Molina <fd.molina@outlook.com>
2023-07-13 23:01:20 -0500

    Adding EPAHub bot

--- src/extractions/EPA-Hub/EPAHubExtractor.py
+++ src/extractions/EPA-Hub/EPAHubExtractor.py
@@ -1,2 +1,28 @@
+"""Class to extract data from EPA Hub API"""
 import requests
 import pandas as pd
+
+
+class EPAHub:
+    def __init__(self):
+        self.URL = "https://www.epa.gov/climateleadership/ghg-emission-factors-hub"
+        self.headers = {
+            "Authority": "www.epa.gov",
+            "Method": "GET",
+            "Path": "/climateleadership/ghg-emission-factors-hub",
+            "Scheme": "https",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Cache-Control": "max-age=0",
+            "If-None-Match": "1689275341",
+            "Sec-Ch-Ua": '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "cross-site",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        }
