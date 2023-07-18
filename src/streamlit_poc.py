3c99869adb2653b6998ed4ceb322711419ce657a
Federico Domínguez Molina <fd.molina@outlook.com>
2023-07-18 17:01:05 -0500

    :bug: payload change

--- src/streamlit_poc.py
+++ src/streamlit_poc.py
@@ -2,9 +2,18 @@ import pandas as pd
 import streamlit as st
 
 # Local Imports
-from extractions.CARB import CARBExtractor
-from extractions.EPAFlight import EPAExtractor
-from extractions.EPAHub import EPAHubExtractor
-from extractions.EMFAC import EMFACExtractor
-from extractions.GoogleEIE import GoogleScraper
-from extractions.USCensus import USCensus
+from extractions.CARB.CARBExtractor import CARBExtractor
+from extractions.EPAFlight.EPAExtractor import EPAExtractor
+from extractions.EPAHub.EPAHubExtractor import EPAHubExtractor
+from extractions.EMFAC.EMFACExtractor import EMFACExtractor
+from extractions.GoogleEIE.GoogleScraper import GoogleScraper
+from extractions.USCensus.USCensus import USCensus
+
+
+# Initialize Extractors
+carb_extractor = CARBExtractor()
+epa_extractor = EPAExtractor()
+epa_hub_extractor = EPAHubExtractor()
+emfac_extractor = EMFACExtractor()
+google_scraper = GoogleScraper()
+uscensus_extractor = USCensus()
